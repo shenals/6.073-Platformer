5de823d2037061453496b0acca368bdc016d1d43
shenals <shenal@mit.edu>
2020-02-18 16:44:39 -0500

    added ui and GameManager

---
+++ 6.073 Platformer/Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{	
+	public Text timer1, timer2, score1, score2, hp1, hp2;
+    public Platformer player1, player2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer1.text = "Time: " + Mathf.Round(Time.timeSinceLevelLoad).ToString();
+        timer2.text = "Time: " + Mathf.Round(Time.timeSinceLevelLoad).ToString();
+        hp1.text = "HP: " + player1.hp.ToString();
+        hp2.text = "HP: " + player2.hp.ToString();
+    }
+}
---
+++ 6.073 Platformer/Assets/loadLevel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class loadLevel : MonoBehaviour
+{
+	//Button play;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LevelLoad(){
+    	SceneManager.LoadScene("SampleScene");
+    }
+}
